14e5f123a87ce1f754ac0a5377a62bbea1bf550f
CarisTech-Technologies <126751015+CarisTech-Technologies@users.noreply.github.com>
2023-08-29 14:15:16 +0300

    second commit

--- frontend/src/components/Shop/AllProducts.jsx
+++ frontend/src/components/Shop/AllProducts.jsx
@@ -7,6 +7,8 @@ import { Link } from "react-router-dom";
 import { getAllProductsShop } from "../../redux/actions/product";
 import { deleteProduct } from "../../redux/actions/product";
 import Loader from "../Layout/Loader";
+import axios from "axios";
+import { server } from "../../server";
 
 const AllProducts = () => {
   const { products, isLoading } = useSelector((state) => state.products);
@@ -18,9 +20,14 @@ const AllProducts = () => {
     dispatch(getAllProductsShop(seller._id));
   }, [dispatch]);
 
-  const handleDelete = (id) => {
-    dispatch(deleteProduct(id));
-    window.location.reload();
+  const handleDelete = async (id) => {
+    await axios
+    .delete(`${server}/product/delete-shop-product/${id}`, { withCredentials: true })
+    .then((res) => {
+      toast.success(res.data.message);
+    });
+
+ // dispatch(getAllUsers());
   };
 
   const columns = [
@@ -81,9 +88,32 @@ const AllProducts = () => {
       renderCell: (params) => {
         return (
           <>
-            <Button onClick={() => handleDelete(params.id)}>
-              <AiOutlineDelete size={20} />
-            </Button>
+            {open && (
+          <div className="w-full fixed top-0 left-0 z-[999] bg-[#00000039] flex items-center justify-center h-screen">
+            <div className="w-[95%] 800px:w-[40%] min-h-[20vh] bg-white rounded shadow p-5">
+              <div className="w-full flex justify-end cursor-pointer">
+                <RxCross1 size={25} onClick={() => setOpen(false)} />
+              </div>
+              <h3 className="text-[25px] text-center py-5 font-Poppins text-[#000000cb]">
+                Are you sure you want to delete this product?
+              </h3>
+              <div className="w-full flex items-center justify-center">
+                <div
+                  className={`${styles.button} text-white text-[18px] !h-[42px] mr-4`}
+                  onClick={() => setOpen(false)}
+                >
+                  cancel
+                </div>
+                <div
+                  className={`${styles.button} text-white text-[18px] !h-[42px] ml-4`}
+                  onClick={() =>  setOpen(false) || handleDelete(params.id)}
+                >
+                  confirm
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
           </>
         );
       },
