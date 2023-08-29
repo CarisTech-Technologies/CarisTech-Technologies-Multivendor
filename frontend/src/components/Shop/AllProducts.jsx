42a53c6451261e7a873b9e1e2e68740fed1e7235
CarisTech-Technologies <126751015+CarisTech-Technologies@users.noreply.github.com>
2023-08-29 15:24:14 +0300

    second commit

--- frontend/src/components/Shop/AllProducts.jsx
+++ frontend/src/components/Shop/AllProducts.jsx
@@ -1,17 +1,15 @@
-import React, { useEffect, useState } from "react";
+import { Button } from "@material-ui/core";
+import { DataGrid } from "@material-ui/data-grid";
+import React, { useEffect } from "react";
+import { AiOutlineDelete, AiOutlineEye } from "react-icons/ai";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { getAllProductsShop } from "../../redux/actions/product";
-import { DataGrid } from "@material-ui/data-grid";
-import { AiOutlineDelete } from "react-icons/ai";
-import { Button } from "@material-ui/core";
-import styles from "../../styles/styles";
-import { RxCross1 } from "react-icons/rx";
-import axios from "axios";
-import { server } from "../../server";
-import { toast } from "react-toastify";
+import { deleteProduct } from "../../redux/actions/product";
+import Loader from "../Layout/Loader";
 
 const AllProducts = () => {
+  const [open, setOpen] = useState(false);
   const { products, isLoading } = useSelector((state) => state.products);
   const { seller } = useSelector((state) => state.seller);
 
@@ -21,61 +19,102 @@ const AllProducts = () => {
     dispatch(getAllProductsShop(seller._id));
   }, [dispatch]);
 
-  const handleDelete = async (id) => {
-    await axios
-    .delete(`${server}/product/delete-shop-product/${id}`, { withCredentials: true })
-    .then((res) => {
-      toast.success(res.data.message);
-    });
-
-  //dispatch(getAllUsers());
+  const handleDelete = (id) => {
+    dispatch(deleteProduct(id));
+    window.location.reload();
+    setOpen(true); // Open the confirmation dialog
   };
 
   const columns = [
-    { field: "id", headerName: "User ID", minWidth: 150, flex: 0.7 },
-
+    { field: "id", headerName: "Product Id", minWidth: 150, flex: 0.7 },
     {
       field: "name",
-      headerName: "name",
-      minWidth: 130,
-      flex: 0.7,
+      headerName: "Name",
+      minWidth: 180,
+      flex: 1.4,
     },
     {
-      field: "email",
-      headerName: "Email",
-      type: "text",
-      minWidth: 130,
-      flex: 0.7,
+      field: "price",
+      headerName: "Price",
+      minWidth: 100,
+      flex: 0.6,
     },
     {
-      field: "role",
-      headerName: "User Role",
-      type: "text",
-      minWidth: 130,
-      flex: 0.7,
+      field: "Stock",
+      headerName: "Stock",
+      type: "number",
+      minWidth: 80,
+      flex: 0.5,
     },
 
     {
-      field: "joinedAt",
-      headerName: "joinedAt",
-      type: "text",
+      field: "sold",
+      headerName: "Sold out",
+      type: "number",
       minWidth: 130,
+      flex: 0.6,
+    },
+    {
+      field: "Preview",
       flex: 0.8,
-    },
-
-    {
-      field: " ",
-      flex: 1,
-      minWidth: 150,
-      headerName: "Delete User",
+      minWidth: 100,
+      headerName: "",
       type: "number",
       sortable: false,
       renderCell: (params) => {
         return (
           <>
-            <Button onClick={() =>  setOpen(true)}>
-              <AiOutlineDelete size={20} />
-            </Button>
+            <Link to={`/product/${params.id}`}>
+              <Button>
+                <AiOutlineEye size={20} />
+              </Button>
+            </Link>
+          </>
+        );
+      },
+    },
+    {
+      field: "Delete",
+      flex: 0.8,
+      minWidth: 120,
+      headerName: "",
+      type: "number",
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <>
+          {open && (
+      <div className="w-full fixed top-0 left-0 z-[999] bg-[#00000039] flex items-center justify-center h-screen">
+        <div className="w-[95%] 800px:w-[40%] min-h-[20vh] bg-white rounded shadow p-5">
+          <div className="w-full flex justify-end cursor-pointer">
+            <RxCross1 size={25} onClick={() => setOpen(false)} />
+          </div>
+          <h3 className="text-[25px] text-center py-5 font-Poppins text-[#000000cb]">
+            Are you sure you want to delete this product?
+          </h3>
+          <div className="w-full flex items-center justify-center">
+            <div
+              className={`${styles.button} text-white text-[18px] !h-[42px] mr-4`}
+              onClick={() => setOpen(false)}
+            >
+              Cancel
+            </div>
+            <div
+              className={`${styles.button} text-white text-[18px] !h-[42px] ml-4`}
+              onClick={() => {
+                setOpen(false);
+                if (deleteProduct) {
+                  handleDelete(deleteProduct);
+                  deleteProduct(null); // Reset product ID after deletion
+                }
+              }}
+            >
+              Confirm
+            </div>
+          </div>
+        </div>
+      </div>
+    )}
           </>
         );
       },
@@ -83,22 +122,24 @@ const AllProducts = () => {
   ];
 
   const row = [];
-  users &&
-    users.forEach((item) => {
+
+  products &&
+    products.forEach((item) => {
       row.push({
         id: item._id,
         name: item.name,
-        email: item.email,
-        role: item.role,
-        joinedAt: item.createdAt.slice(0, 10),
+        price: "Ksh " + item.discountPrice,
+        Stock: item.stock,
+        sold: item?.sold_out,
       });
     });
 
   return (
-    <div className="w-full flex justify-center pt-5">
-      <div className="w-[97%]">
-        <h3 className="text-[22px] font-Poppins pb-2">All Users</h3>
-        <div className="w-full min-h-[45vh] bg-white rounded">
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="w-full mx-8 pt-1 mt-10 bg-white">
           <DataGrid
             rows={row}
             columns={columns}
@@ -107,34 +148,8 @@ const AllProducts = () => {
             autoHeight
           />
         </div>
-        {open && (
-          <div className="w-full fixed top-0 left-0 z-[999] bg-[#00000039] flex items-center justify-center h-screen">
-            <div className="w-[95%] 800px:w-[40%] min-h-[20vh] bg-white rounded shadow p-5">
-              <div className="w-full flex justify-end cursor-pointer">
-                <RxCross1 size={25} onClick={() => setOpen(false)} />
-              </div>
-              <h3 className="text-[25px] text-center py-5 font-Poppins text-[#000000cb]">
-                Are you sure you want to delete this user?
-              </h3>
-              <div className="w-full flex items-center justify-center">
-                <div
-                  className={`${styles.button} text-white text-[18px] !h-[42px] mr-4`}
-                  onClick={() => setOpen(false)}
-                >
-                  cancel
-                </div>
-                <div
-                  className={`${styles.button} text-white text-[18px] !h-[42px] ml-4`}
-                  onClick={() =>  setOpen(false) || handleDelete(userId)}
-                >
-                  confirm
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
