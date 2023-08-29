06503bb625f27ed093f63597f7d819cab994671a
CarisTech-Technologies <126751015+CarisTech-Technologies@users.noreply.github.com>
2023-08-29 15:45:36 +0300

    second commit

--- frontend/src/components/Shop/AllProducts.jsx
+++ frontend/src/components/Shop/AllProducts.jsx
@@ -1,6 +1,6 @@
 import { Button } from "@material-ui/core";
 import { DataGrid } from "@material-ui/data-grid";
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { AiOutlineDelete, AiOutlineEye } from "react-icons/ai";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
@@ -9,9 +9,11 @@ import { deleteProduct } from "../../redux/actions/product";
 import Loader from "../Layout/Loader";
 
 const AllProducts = () => {
-  const [open, setOpen] = useState(false);
   const { products, isLoading } = useSelector((state) => state.products);
   const { seller } = useSelector((state) => state.seller);
+  
+  const [open, setOpen] = useState(false); // State to control the popup
+  const [productIdToDelete, setProductIdToDelete] = useState(null); // State to track the product ID being deleted
 
   const dispatch = useDispatch();
 
@@ -21,58 +23,11 @@ const AllProducts = () => {
 
   const handleDelete = (id) => {
     dispatch(deleteProduct(id));
-    window.location.reload();
-    setOpen(true); // Open the confirmation dialog
+    setOpen(false);
   };
 
   const columns = [
-    { field: "id", headerName: "Product Id", minWidth: 150, flex: 0.7 },
-    {
-      field: "name",
-      headerName: "Name",
-      minWidth: 180,
-      flex: 1.4,
-    },
-    {
-      field: "price",
-      headerName: "Price",
-      minWidth: 100,
-      flex: 0.6,
-    },
-    {
-      field: "Stock",
-      headerName: "Stock",
-      type: "number",
-      minWidth: 80,
-      flex: 0.5,
-    },
-
-    {
-      field: "sold",
-      headerName: "Sold out",
-      type: "number",
-      minWidth: 130,
-      flex: 0.6,
-    },
-    {
-      field: "Preview",
-      flex: 0.8,
-      minWidth: 100,
-      headerName: "",
-      type: "number",
-      sortable: false,
-      renderCell: (params) => {
-        return (
-          <>
-            <Link to={`/product/${params.id}`}>
-              <Button>
-                <AiOutlineEye size={20} />
-              </Button>
-            </Link>
-          </>
-        );
-      },
-    },
+    // ... (your existing column definitions)
     {
       field: "Delete",
       flex: 0.8,
@@ -83,38 +38,12 @@ const AllProducts = () => {
       renderCell: (params) => {
         return (
           <>
-          {open && (
-      <div className="w-full fixed top-0 left-0 z-[999] bg-[#00000039] flex items-center justify-center h-screen">
-        <div className="w-[95%] 800px:w-[40%] min-h-[20vh] bg-white rounded shadow p-5">
-          <div className="w-full flex justify-end cursor-pointer">
-            <RxCross1 size={25} onClick={() => setOpen(false)} />
-          </div>
-          <h3 className="text-[25px] text-center py-5 font-Poppins text-[#000000cb]">
-            Are you sure you want to delete this product?
-          </h3>
-          <div className="w-full flex items-center justify-center">
-            <div
-              className={`${styles.button} text-white text-[18px] !h-[42px] mr-4`}
-              onClick={() => setOpen(false)}
-            >
-              Cancel
-            </div>
-            <div
-              className={`${styles.button} text-white text-[18px] !h-[42px] ml-4`}
-              onClick={() => {
-                setOpen(false);
-                if (deleteProduct) {
-                  handleDelete(deleteProduct);
-                  deleteProduct(null); // Reset product ID after deletion
-                }
-              }}
-            >
-              Confirm
-            </div>
-          </div>
-        </div>
-      </div>
-    )}
+            <Button onClick={() => {
+              setProductIdToDelete(params.id);
+              setOpen(true);
+            }}>
+              <AiOutlineDelete size={20} />
+            </Button>
           </>
         );
       },
@@ -147,6 +76,39 @@ const AllProducts = () => {
             disableSelectionOnClick
             autoHeight
           />
+        </div>
+      )}
+
+      {/* Delete Confirmation Popup */}
+      {open && (
+        <div className="w-full fixed top-0 left-0 z-[999] bg-[#00000039] flex items-center justify-center h-screen">
+          <div className="w-[95%] 800px:w-[40%] min-h-[20vh] bg-white rounded shadow p-5">
+            <div className="w-full flex justify-end cursor-pointer">
+              <RxCross1 size={25} onClick={() => setOpen(false)} />
+            </div>
+            <h3 className="text-[25px] text-center py-5 font-Poppins text-[#000000cb]">
+              Are you sure you want to delete this product?
+            </h3>
+            <div className="w-full flex items-center justify-center">
+              <div
+                className={`${styles.button} text-white text-[18px] !h-[42px] mr-4`}
+                onClick={() => setOpen(false)}
+              >
+                Cancel
+              </div>
+              <div
+                className={`${styles.button} text-white text-[18px] !h-[42px] ml-4`}
+                onClick={() => {
+                  setOpen(false);
+                  if (productIdToDelete) {
+                    handleDelete(productIdToDelete);
+                  }
+                }}
+              >
+                Confirm
+              </div>
+            </div>
+          </div>
         </div>
       )}
     </>
